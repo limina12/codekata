375e6b8933e9b5a311130cff5f24f1796ea04122
limina12 <34646822+limina12@users.noreply.github.com>
2018-01-15 10:35:07 -0800

    Create armstrong no between two intervals.cpp

---
+++ Beginner/armstrong no between two intervals.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n1,n2,i,temp=0,rem=0,sum=0;
+	cin>>n1>>n2;
+	for(i=n1+1;i<n2;i++)
+	{
+	    temp=i;
+	    sum=0;
+	    rem=0;
+	    while(temp>0)
+	    {
+	        rem=temp%10;
+	        sum=sum+(rem*rem*rem);
+	        temp/=10;
+	    }
+	    if(i==sum)
+	    {
+	        cout<<i;
+	    }
+	}
+	return 0;
+}
