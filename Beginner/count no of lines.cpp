dcfefb6f9dc455089bd6c27d421e3fe6255ee3cc
limina12 <34646822+limina12@users.noreply.github.com>
2018-01-16 10:52:09 -0800

    Create count no of lines.cpp

---
+++ Beginner/count no of lines.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+
+int main() {
+	int i,c=0;
+	char str[100];
+	cin.getline(str,100);
+	for(i=0;str[i]!='\0';i++)
+	{
+	    if(str[i]=='.')
+	    {
+	    c++;
+	    }
+	}
+	cout<<c;
+	return 0;
+}
