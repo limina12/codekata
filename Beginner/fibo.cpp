1b642fc5357c12ff7cae9c147192d6ec1cd96984
limina12 <34646822+limina12@users.noreply.github.com>
2018-02-13 09:35:00 -0800

    Create fibo.cpp

---
+++ Beginner/fibo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a=0,b=1,c,i,n;
+	cin>>n;
+	for(i=1;i<=n;i++)
+	{
+	    cout<<a<<" ";
+	    c=a+b;
+	    a=b;
+	    b=c;
+	}
+	return 0;
+}
