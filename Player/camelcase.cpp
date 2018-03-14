8c844e1349ec4d44f3938cd1ed06cc6166ac0776
limina12 <34646822+limina12@users.noreply.github.com>
2018-03-14 10:31:27 -0700

    Create camelcase.cpp

---
+++ Player/camelcase.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main() {
+	char str[20];
+	int len,i;
+	cin.getline(str,20);
+	len=strlen(str);
+	for(i=0;i<len;i++)
+	{
+	    if(str[i]==str[0])
+	    {
+	        str[i]=toupper(str[i]);
+	    }
+	    if(str[i]==' ')
+	    {
+	        str[i+1]=toupper(str[i+1]);
+	        i++;
+	    }
+	}
+	for(i=0;i<len;i++)
+	cout<<str[i];
+	return 0;
+}
